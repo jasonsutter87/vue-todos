58c65b5181e901276a266e8216994b85f2693312
JasonS <jason@infostarproductions.com>
2019-04-18 12:29:11 -0700

    Refactored code to use component

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+Vue.component('todo-form', {
+  props: [],
+  template: `
+  <div class="mx-auto text-center">
+    <input type="text" name="newTodo" v-model="newTodo" placeholder="Write Your Todo." class="w-75 form-size">
+    <button type="submit" name="submit" value="submit" v-on:click='onSubmit(newTodo)' class="form-size">Submit</button>
+  </div>
+  `,
+  data() {
+    return {
+        newTodo: ''
+    }
+  },
+  methods: {
+    onSubmit(listItem) {
+      this.$emit('add-to-todos', listItem)
+    }
+  }
+})
+
 let app = new Vue({
   el: '#app',
   data() {
